9d2e56aacc5b452850f43fe2a2e72a299a992170
Ronit Rangwani <ronitrangwani@gmail.com>
2024-02-03 22:54:19 +0530

    created profile page ui

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -3,6 +3,8 @@ import { Grid } from "@mui/material";
 import Navigation from "../Navigation/Navigation";
 import HomeSection from "../HomeSection/HomeSection";
 import HappeningSection from "../HappeningSection/HappeningSection";
+import { Route, Routes } from "react-router-dom";
+import Profile from "../Profile/Profile";
 
 const HomePage = () => {
   return (
@@ -14,9 +16,12 @@ const HomePage = () => {
         item
         xs={12}
         lg={6}
-        className="px-5 lg:px-9 hidden lg:block w-full relative"
+        className="px-5 lg:px-9 lg:block w-full relative"
       >
-        <HomeSection />
+        <Routes>
+          <Route path="/" element={<HomeSection />}></Route>
+          <Route path="/profile/:id" element={<Profile />}></Route>
+        </Routes>
       </Grid>
       <Grid item xs={0} lg={3} className="lg:block w-full relative">
         <HappeningSection />
---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,151 @@
+import React, { useState } from "react";
+import KeyboardBackspaceIcon from "@mui/icons-material/KeyboardBackspace";
+import { useNavigate } from "react-router-dom";
+import { Avatar, Box, Button, Tab } from "@mui/material";
+import TabContext from "@mui/lab/TabContext";
+import TabList from "@mui/lab/TabList";
+import TabPanel from "@mui/lab/TabPanel";
+import BusinessCenterIcon from "@mui/icons-material/BusinessCenter";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import CalendarMonthIcon from "@mui/icons-material/CalendarMonth";
+import TweetCard from "../HomeSection/TweetCard";
+
+const Profile = () => {
+  const [tabValue, setTabValue] = useState(1);
+  const navigate = useNavigate();
+
+  const handleBack = () => {
+    navigate(-1);
+  };
+
+  const handleOpenProfileModel = () => {
+    console.log("open profile model");
+  };
+
+  const handleFollowUser = () => {};
+
+  const handleTabChange = (event, newValue) => {
+    setTabValue(newValue);
+    if (newValue === 4) {
+      console.log("likes tweet");
+    } else if (newValue === 1) {
+      console.log("user tweets");
+    }
+  };
+  return (
+    <div>
+      <section className={`z-50 flex items-center sticky top-0 bg-opacity-95`}>
+        <KeyboardBackspaceIcon
+          className="cursor-pointer"
+          onClick={handleBack}
+        />
+        <h1 className="py-5 text-x font-bold opacity-90 ml-5">Ron Ran</h1>
+      </section>
+      <section>
+        <img
+          className="w-[100%] h-[15rem] object-cover"
+          src="https://cdn.pixabay.com/photo/2016/06/02/02/33/triangles-1430105_960_720.png"
+          alt=""
+        />
+      </section>
+      <section className="pl-6">
+        <div className="flex justify-between item-start mt-5 h-[5rem]">
+          <Avatar
+            className="transform -translate-y-24"
+            alt="ron ran"
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR5QX51-SXSdd_3NLB1hK4KR0lQrkoKlPsPRG8f18ODpBbLovPMJ_L9z55JCbBo9TrCduw&usqp=CAU"
+            sx={{ height: "10rem", width: "10rem", border: "4px solid white" }}
+          />
+
+          {true ? (
+            <Button
+              variant="contained"
+              sx={{
+                borderRadius: "20px",
+                height: "40px",
+              }}
+              onClick={handleOpenProfileModel}
+            >
+              Edit Profile
+            </Button>
+          ) : (
+            <Button
+              variant="contained"
+              sx={{
+                borderRadius: "20px",
+                height: "40px",
+              }}
+              onClick={handleFollowUser}
+            >
+              {true ? "Follow" : "Unfollow"}
+            </Button>
+          )}
+        </div>
+        <div>
+          <div className="flex items-center">
+            <h1 className="font-bold text-lg">Ron Ran</h1>
+            {true && <img className="ml-2 w-5 h-5" src="" alt="" />}
+          </div>
+          <h1 className="text-gray-500">@ronran</h1>
+        </div>
+        <div className="mt-2 space-y-3">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
+            fuga commodi nobis iste!
+          </p>
+          <div className="py-1 flex space-x-5">
+            <div className="flex items-center text-gray-500">
+              <BusinessCenterIcon />
+              <p className="ml-2">Education</p>
+            </div>
+            <div className="flex items-center text-gray-500">
+              <LocationOnIcon />
+              <p className="ml-2">Location</p>
+            </div>
+            <div className="flex items-center text-gray-500">
+              <CalendarMonthIcon />
+              <p className="ml-2">Joined Jun 2022</p>
+            </div>
+          </div>
+          <div className="flex items-center space-x-5">
+            <div className="flex items-center space-x-1 font-semibold">
+              <span>90</span>
+              <span className="text-gray-500">Following</span>
+            </div>
+            <div className="flex items-center space-x-1 font-semibold">
+              <span>590</span>
+              <span className="text-gray-500">Followers</span>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="py-5">
+        <Box sx={{ width: "100%", typography: "body1" }}>
+          <TabContext value={tabValue}>
+            <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+              <TabList
+                onChange={handleTabChange}
+                aria-label="lab API tabs example"
+              >
+                <Tab label="Tweets" value="1" />
+                <Tab label="Replies" value="2" />
+                <Tab label="Media" value="3" />
+                <Tab label="Likes" value="4" />
+              </TabList>
+            </Box>
+            <TabPanel value="1">
+              {[1, 1, 1, 1, 1].map((item) => (
+                <TweetCard />
+              ))}
+            </TabPanel>
+            <TabPanel value="2">users reply</TabPanel>
+            <TabPanel value="3">Media</TabPanel>
+            <TabPanel value="4">Likes</TabPanel>
+          </TabContext>
+        </Box>
+      </section>
+    </div>
+  );
+};
+
+export default Profile;
